6d5ef803954272f75ad0bf5ffd19a593b539ff01
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-08-24 15:56:54 -0300

    Add files via upload

---
+++ src/Js/nova-janela.js
@@ -0,0 +1,13 @@
+//abrir em nova aba
+
+function openFormulario() {
+    window.open('Tabular.html', 'Formulário', 'width=362,height=615.333');
+  }
+
+  function openBlocoNotas() {
+    window.open('bloco-notas.html', 'Bloco de Notas', 'width=362,height=615.333');
+}
+
+function openPostagem() {
+  window.open('src/docs/postagem-correios.pdf', 'Postagem/Correios', 'width=362,height=615.333');
+}
