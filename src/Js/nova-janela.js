902b2898ce78d826927966ff13a2328e154a400c
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-09-13 12:14:39 -0300

    Update nova-janela.js

--- src/Js/nova-janela.js
+++ src/Js/nova-janela.js
@@ -12,7 +12,6 @@ function openPostagem() {
   window.open('src/docs/postagem-correios.pdf', 'Postagem/Correios', 'width=584,height=608');
 }
 
-function openTransportadora(https://docs.google.com/spreadsheets/d/1IJ6pjwe86F525uJHlr-3StWheA9d8ARxi6LgyxLdSd8/edit#gid=338307967&range=A1:D13) {
-    window.open(https://docs.google.com/spreadsheets/d/1IJ6pjwe86F525uJHlr-3StWheA9d8ARxi6LgyxLdSd8/edit#gid=338307967&range=A1:D13, 'Planilha', 'width=515.333,height=494.667');
+function openTransportadora() {
+    window.open('https://docs.google.com/spreadsheets/d/1IJ6pjwe86F525uJHlr-3StWheA9d8ARxi6LgyxLdSd8/edit#gid=338307967&range=A1:D13', '_blank', 'width=515.333,height=494.667');
 }
-
